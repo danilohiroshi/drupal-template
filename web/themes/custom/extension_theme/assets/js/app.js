8f6347aca2de9dc91c7b894c5e26f17da110a723
Danilo Yamashita <dyamash2@its.jnj.com>
2021-05-16 11:57:59 -0300

    Theme

--- web/themes/custom/extension_theme/assets/js/app.js
+++ web/themes/custom/extension_theme/assets/js/app.js
@@ -1,46 +1,19 @@
-require("./bootstrap");
 
-window.Vue = require("vue");
+window._ = require('lodash');
 
-// Vue.component("contact", require("./components/Contact.vue"));
+try {
+    window.$ = window.jQuery = require('jquery');
 
-const app = new Vue({
-  el: "#app"
-});
+    require('bootstrap');
+} catch (e) {}
 
-$(document).ready(function() {
-  $('a[href*="#"]')
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
-      if (
-        location.pathname.replace(/^\//, "") ==
-          this.pathname.replace(/^\//, "") &&
-        location.hostname == this.hostname
-      ) {
-        var target = $(this.hash);
-        target = target.length
-          ? target
-          : $("[name=" + this.hash.slice(1) + "]");
-        if (target.length) {
-          event.preventDefault();
-          $("html, body").animate(
-            {
-              scrollTop: target.offset().top
-            },
-            1000,
-            function() {
-              var $target = $(target);
-              $target.focus();
-              if ($target.is(":focus")) {
-                return false;
-              } else {
-                $target.attr("tabindex", "-1");
-                $target.focus();
-              }
-            }
-          );
-        }
-      }
-    });
-});
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+import bootstrap from 'bootstrap'
+import moment from 'moment'
+import aos from 'aos'
+aos.init();
+
+
